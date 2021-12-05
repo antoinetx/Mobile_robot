54d5f48bc0a7f8710e59babc34ee426979870307
Antoine Perrin <antoinepierreperrin@gmail.com>
2021-12-05 14:31:41 +0100

    frame capture and grid initialisation

--- Map.py
+++ Map.py
@@ -1,21 +1,92 @@
 import numpy as np
-
+import math
+import matplotlib.pyplot as plt
 
 
 class Map :
-    def __init__(self, lenght_in_m, nb_square_per_side):
-            self.lenght_m = lenght_in_m
-            self.nb_square_per_side = nb_square_per_side
-            self.grid = np.empty((nb_square_per_side,nb_square_per_side))
-            self.square_lenght = self.lenght_m/self.nb_square_per_side
-            
-            
+    def __init__(self, lenght_in_m, wanted_nb_square_per_side):
+        """
+        Init the Map object
+        :param lenght_in_m: lenth of the smallest side in m 
+        :wanted_nb_square_per_side: the approximately wanted nb of square for smallest side)
+        """
+        self._lenght_m = lenght_in_m #lenght of the smallest size
+        self._wanted_nb_square_by_side = wanted_nb_square_per_side
+        self._grid_init = False
+    
 
     def get_map(self):
-        return self.grid
+        if self._grid_init:
+            return self._grid
+        else:
+            print("No grid yet. Please init the grid")
 
-    def update_map(self, new_grid):
-        self.grid = new_grid
+    #def update_map(self, new_grid):
+    #    self.grid = new_grid
 
     def get_lenght(self):
-        return self.nb_square_per_side
+        return self._nb_square_by_side
+    
+    def init_grid(self, frame, r_tresh, g_tresh, b_tresh):
+        """
+        Create an OccupancyGridMap from a video frame
+        :param frame: the video frame 
+        :param r_tresh, g_tresh, b_tresh: the treshold to choose wich color to extract (each tes between 0and 255)
+        :Save in map._grid: the created grid map
+        """
+        self._square_pixe_size = math.floor(len(frame)/self._wanted_nb_square_by_side)
+        self._nb_square_by_side = math.ceil(len(frame)/self._square_pixe_size)
+        self._square_size_m = self._lenght_m/self._nb_square_by_side # Not the exact value 
+        
+        square_pixe_size = self._square_pixe_size
+        nb_square_by_side = self._nb_square_by_side
+        
+        data_j = np.zeros((len(frame),math.ceil(len(frame[0])/square_pixe_size))) 
+        data_i = np.zeros((math.ceil(nb_square_by_side),math.ceil(len(frame[0])/square_pixe_size)))      
+        
+        j_pixel_state = 0
+        i_pixel_state = 0
+        
+        for i in range(len(frame)):
+            for j in range(len(frame[0])):
+                r = frame[i,j,0]
+                g = frame[i,j,1]
+                b = frame[i,j,2]
+                
+                if(((r >= r_tresh[0]) & (r <= r_tresh[1])) & ((g >= g_tresh[0]) & (g <= g_tresh[1])) & ((b >= b_tresh[0]) & (b <= b_tresh[1]))):
+                    j_pixel_state = 1
+                if (j % square_pixe_size == square_pixe_size-1):
+                    
+                    data_j[i,int(((j+1)/square_pixe_size)-1)] = j_pixel_state
+                    j_pixel_state = 0
+                elif ((j == len(frame[0]) - 1)):
+                    data_j[i,math.ceil((j+1)/square_pixe_size)-1] = j_pixel_state
+        
+            
+        print(data_j)
+
+        #print(len(data_j[1]))
+        print("half done")
+        for j in range(len(data_j[1])):
+            for i in range(len(frame)):
+                if(data_j[i,j] == 1):
+                    i_pixel_state = 1
+                if (i % square_pixe_size == square_pixe_size-1):
+                    data_i[(nb_square_by_side-1)-int(((i+1)/square_pixe_size)-1),j] = i_pixel_state
+                    i_pixel_state = 0     
+                elif ((i == len(frame) - 1)):
+                    data_i[(nb_square_by_side-1)-(math.ceil((i+1)/square_pixe_size))-1,j] = i_pixel_state   
+                    
+                    
+        # Save the data in the map variable _grid
+        self._grid = data_i
+        self._grid_init = True
+        
+        
+    def grid_show(self):
+        """
+        Plot the _grid
+        """
+        plt.imshow(self._grid, vmin=0, vmax=1, origin='lower', interpolation='none', alpha=1)
+        plt.draw()
+        plt.show()
