6fbdb1eacf64ef2ee6820b40cfabe4598b842685
Alicia-epfl <79908084+Alicia-epfl@users.noreply.github.com>
2021-12-08 00:20:36 +0100

    MAIN: Avoid is working well <3

--- Local_navigation.py
+++ Local_navigation.py
@@ -5,70 +5,17 @@
 # 
 # Author: Alicia Mauroux, Robotic MA1, Fall 2021
 
-# Connecting to Thymio
-
-# In[1]:
-
-
-
-
-# In order to use sync_to_async
-
-# In[2]:
-
-
-
-
-
-# In order to do the tests:
-
-# In[3]:
-
 from asgiref.sync import sync_to_async
 import tdmclient
 
-
-# # Functions in order to test the local avoidance
-
-# In[4]:
-
-
-@tdmclient.notebook.sync_var
-def motors(l_speed=500, r_speed=500, verbose=False):
-    """
-    Sets the motor speeds of the Thymio 
-    param l_speed: left motor speed
-    param r_speed: right motor speed
-    param verbose: whether to print status messages or not
-    """
-    global motor_left_target, motor_right_target
-    # Printing the speeds if requested
-    if verbose:
-        print("\t\t Setting speed : ", l_speed, r_speed)
-    motor_left_target = l_speed
-    motor_right_target = r_speed
-
-
-
-# # Local Navigation
-
-# ### Global variables and constants
-
-# In[5]:
-
-
+# Global variables and constants
 LED = 32
 SMALL_LED = 2
 
 left_obstacle = False
 right_obstacle = False
-Bloqued = False
-side = 2 #0 : Thymio turned left / 1: Thymio turned right
-run = 0
 
-
-# In[6]:
-
+#LEDS Control
 
 @tdmclient.notebook.sync_var
 def light_em_up(avoid=0,right=0,wait=0):
@@ -82,24 +29,13 @@ def light_em_up(avoid=0,right=0,wait=0):
         leds_top = [LED,0,LED]
     else:
         leds_top = [0,LED,LED]
+
+##################
+# MAIN FUNCTIONS
+##################
         
-
-
-# In[7]:
-
-
-@tdmclient.notebook.sync_var
-def play_music():
-    global sound_system
-    sound_system = [5]
-
-
 # ### Check cars
 # This function will check if there's something in front of Thymio. If there's, it will return **TRUE** and take the control of the Thymio. If there's nothing, it will return **FALSE** and let the control to optimal path
-
-# In[8]:
-
-
 @tdmclient.notebook.sync_var
 def check_cars(Tres_high=1400, Tres_mid_side_high=1500, Tres_low=1500, Tres_mid_side_low=1000, Tres_side_high=2000, prox_horizonta=0):
 
@@ -111,19 +47,10 @@ def check_cars(Tres_high=1400, Tres_mid_side_high=1500, Tres_low=1500, Tres_mid_
     #There's nothting, Thymio continue its normal ways 
 
 
-# In[9]:
-
-
-
-
-
 # ### Avoid function
 # This function will check to the left/right if there's a "Thymio-car" so our Thymio can avoid the car in front of it. If there's a "Thymio-car" in front of it and on its left/right, our Thymio will wait until the way to the left/right is free again.
 # 
 # **We call this function when check_cars() = True otherwise we call the logical path**
-
-# In[11]:
-
 
 @tdmclient.notebook.sync_var
 def avoid_obstacle(Tres_side_high=1200, Tres_side_low=500, Tres_low=1500, Tres = 100, prox_horizonta=0):
@@ -181,7 +108,7 @@ def avoid_obstacle(Tres_side_high=1200, Tres_side_low=500, Tres_low=1500, Tres =
         if (obst[0]<Tres_side_low):
             if (obst[1]<Tres_side_low):
                 light_em_up(avoid=0)
-                return False
+                return speed_l, speed_r, False
     else:
         if (speed_l > speed_r + Tres):
             speed_r = 0
@@ -190,38 +117,5 @@ def avoid_obstacle(Tres_side_high=1200, Tres_side_low=500, Tres_low=1500, Tres =
 
     print(speed_l, speed_r)
     light_em_up(avoid=1)
-    motors(speed_l,speed_r)
-    return True
     
-    #to do the counter later!
-
-
-# ### Ideas about how to implement
-
-# In[12]:
-
-
-async def control():
-    global run #bool
-    
-    while True:
-        if run:
-            run = avoid_obstacle()
-        else:
-            run = check_cars()
-            motors(100,100)
-        sleep(0.5)
-
-
-# In[ ]:
-
-
-
-    
-
-
-# In[ ]:
-
-
-
-
+    return speed_l, speed_r, True
