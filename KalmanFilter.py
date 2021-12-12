d3360886c64ea316551e59daff5998e63c3c80e1
xav-nal <xavier.nal@epfl.ch>
2021-12-12 05:55:43 +0100

    update main test3

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+b=1
+
 class KalmanFilter(object):
     def __init__(self, dt, point):
         self.dt=dt
@@ -18,28 +20,34 @@ class KalmanFilter(object):
                           [0, 1, 0, 0],
                           [0, 0, 1, 0],
                           [0, 0, 0, 1]])
-
-        self.Q=np.matrix([[1, 0, 0, 0],
-                          [0, 1, 0, 0],
-                          [0, 0, 1, 0],
-                          [0, 0, 0, 1]])
+        v=1
+        b=1
+        self.Q=np.matrix([[v, 0, 0, 0],
+                          [0, v, 0, 0],
+                          [0, 0, b, 0],
+                          [0, 0, 0, b]])
         
-        v = 1        
+        v=1
+        b=1E-5
+        #v=1       
         self.R=np.matrix([[v, 0, 0, 0],
                           [0, v, 0, 0],
-                          [0, 0, v, 0],
-                          [0, 0, 0, v]])
+                          [0, 0, b, 0],
+                          [0, 0, 0, b]])
 
         self.P=np.eye(self.A.shape[1])
 
     def predict(self):
+        print('les matrices A et E sont ', self.A,self.E)
         self.E=np.dot(self.A, self.E)
+        print(' la matrice E est'. self.E)
         # Calcul de la covariance de l'erreur
         self.P=np.dot(np.dot(self.A, self.P), self.A.T)+self.Q
        # print('la taille de P est', self.P.shape)
         return self.E
 
     def update(self, z):
+        print('la valeur  b est',b)
         # Calcul du gain de Kalman
         #print('le vecteur z est', z)
         #print('la taille de z est', z.shape)
